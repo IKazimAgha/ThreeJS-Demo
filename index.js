352b39ea446fca2273d1688d808663d193c4016c
Kazim Agha <kazim.agha@systemsltd.com>
2022-12-16 15:48:14 +0500

    Adding spot light for orbit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+renderer.shadowMap.enabled = true;
+
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
@@ -30,30 +32,70 @@ scene.add(box)
 
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: THREE.DoubleSide
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper)
 
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0000FF,
     wireframe: false,
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere)
+scene.add(sphere);
+sphere.position.set(-10, 0, 0);
+sphere.castShadow = true;
 
-sphere.position.set(-10, 0, -10)
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight)
+
+const directionLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+scene.add(directionLight);
+directionLight.position.set(-30, 50, 0);
+directionLight.castShadow = true;
+directionLight.shadow.camera.bottom = -12;
+
+const dlHelper = new THREE.DirectionalLightHelper(directionLight, 5);
+scene.add(dlHelper);
+
+const dlShadowHelper = new THREE.CameraHelper(directionLight.shadow.camera);
+scene.add(dlShadowHelper);
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01,
+}
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+    sphere.material.color.set(e)
+})
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e
+})
+
+gui.addColor(options, 'speed', 0, 0.1);
+
+let step = 0;
+
 
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
 
     renderer.render(scene, camera)
 }
